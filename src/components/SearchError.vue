49f16dae3cadcbbced629cded7003b2ec8ea13c2
Nick <133665239+BpJ-inx@users.noreply.github.com>
2023-05-17 09:53:01 +0300

    correction of styles

--- src/components/SearchError.vue
+++ src/components/SearchError.vue
@@ -3,7 +3,7 @@
         <div class=" w-full px-16  flex items-center justify-center">
             <div
                 class="error  border  flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8 rounded-lg shadow-2xl">
-                <p class="text-2xl md:text-3xl lg:text-5xl font-bold tracking-wider  mt-4">No Search Result</p>
+                <p class="text-2xl text-center lg:text-5xl font-bold tracking-wider  mt-4">No Search Result</p>
                 <p class="textError  mt-4 pb-4 border-b-2 text-center"></p>
 
             </div>
