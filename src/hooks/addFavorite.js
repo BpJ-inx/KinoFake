64cd736819a687ef23890c72b9e4ed040c1c4c76
Nick <133665239+BpJ-inx@users.noreply.github.com>
2023-07-06 10:54:36 +0300

    possibility to remove from favorites (changing a button, icon)

--- src/hooks/addFavorite.js
+++ src/hooks/addFavorite.js
@@ -2,14 +2,22 @@
 
 let userFavFilms = JSON.parse(localStorage.getItem(`favorite.${localStorage.getItem('logNAME')}`))
 
+export let nameFavBut = 'To favorites'
 export let isFav = false
 
 export function addFavoriteFilm() {
 
+    userFavFilms = JSON.parse(localStorage.getItem(`favorite.${localStorage.getItem('logNAME')}`))
+
     if (!userFavFilms.includes(document.querySelector('.idFilm').innerHTML)) {
         localStorage.setItem(`favorite.${localStorage.getItem('logNAME')}`, JSON.stringify(userFavFilms + '.' + document.querySelector('.idFilm').innerHTML))
         document.querySelector('.fav__star').style.display = 'flex'
+        document.querySelector('.buttonFav').innerHTML = 'From Favorites'
 
+    }else{
+        document.querySelector('.buttonFav').innerHTML = 'To favorites'
+        document.querySelector('.fav__star').style.display = 'none'
+        localStorage.setItem(`favorite.${localStorage.getItem('logNAME')}`, JSON.stringify(userFavFilms.replace('.'+ document.querySelector('.idFilm').innerHTML,'')))
     }
 
 }
@@ -19,11 +27,14 @@ export function checkIsFav() {
     if (userFavFilms) {
         if (userFavFilms.includes(localStorage.getItem(`filmID`))) {
             isFav = true
+            nameFavBut = 'From Favorites'
         } else {
             isFav = false
+            nameFavBut = 'To favorites'
         }
     } else {
         isFav = false
+        nameFavBut = 'To favorites'
     }
 
 }
