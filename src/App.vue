5e1afad87395783bf5d77239704d8fb2a840e506
Nick <133665239+BpJ-inx@users.noreply.github.com>
2023-07-12 14:50:20 +0300

    unique id of users who allowed the use of cookies. can leave their comments. the rest can`t

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,28 @@ export default {
   components: {
     AuthoAndRegForm,
   },
+  mounted() {
+    localStorage.setItem('freeIDUser', 0) ///уникальные id пользоавтелей которые бы хронились на сервере...
+
+    if (localStorage.getItem('guestID') || document.cookie.match(new RegExp("(?:^|; )" + 'cookie__user_id'.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"))) {
+      localStorage.setItem('guestID', `guest${document.cookie.match(new RegExp("(?:^|; )" + 'cookie__user_id'.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"))[1]}`)
+    }
+
+    if (!document.cookie.match(new RegExp("(?:^|; )" + 'cookie__user_id'.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"))) {
+      let freeIDUser = localStorage.getItem('freeIDUser')
+      let ask = confirm("We use cookies. Its OK?")
+      if (ask) {
+        document.cookie = (`cookie__user_id= ${freeIDUser}`)
+        localStorage.setItem('guestID', `guest${document.cookie.match(new RegExp("(?:^|; )" + 'cookie__user_id'.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"))[1]}`)
+        localStorage.setItem(`reviews.${localStorage.getItem('guestID')}`,JSON.stringify([]))
+        localStorage.setItem('freeIDUser', +localStorage.getItem('freeIDUser') + 1)
+      } else {
+        localStorage.removeItem('guestID')
+      }
+    }
+
+  },
 }
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- src/hooks/reviews.js
+++ src/hooks/reviews.js
@@ -3,56 +3,67 @@ import { ref } from 'vue'
 export const arrayReviews = ref([])
 
 export function addReview() {
-    let descriptionRev = document.querySelector('.reviews__text_area').value
+    if (localStorage.getItem('logNAME') || localStorage.getItem('guestID')) {
+        let descriptionRev = document.querySelector('.reviews__text_area').value
 
-    if (descriptionRev) {
-        let userName = ''
-        let idFilm = localStorage.getItem('filmID')
-        let x = 0
+        if (descriptionRev) {
+            let userName = ''
+            let idFilm = localStorage.getItem('filmID')
+            let x = 0
 
-        if (localStorage.getItem('logNAME')) {
-            userName = localStorage.getItem('logNAME')
-        } else {
-            userName = 'guest'
-        }
-
-        let review = {
-            Name: userName,
-            ID: idFilm,
-            Description: descriptionRev
-        }
-
-        if (localStorage.getItem('lengthRev')) {
-            x = +localStorage.getItem('lengthRev')
-        } else {
-            localStorage.setItem('lengthRev', 0)
-
-        }
-        for (let i = 0; i < x + 1; ++i) {
-            if (!localStorage.getItem(`Rev.${i}`)) {
-
-                localStorage.setItem(`Rev.${[i]}`, JSON.stringify(review))
-
-                let reviewsUser = JSON.parse(localStorage.getItem(`reviews.${localStorage.getItem('logNAME')}`))
-                reviewsUser.push(x)
-                localStorage.setItem(`reviews.${localStorage.getItem('logNAME')}`, JSON.stringify(reviewsUser))
-
-                if (localStorage.getItem(`reviewsFilm${idFilm}`)) {
-                    let reviewsFilm = JSON.parse(localStorage.getItem(`reviewsFilm${idFilm}`))
-                    reviewsFilm.push(x)
-                    localStorage.setItem(`reviewsFilm${idFilm}`, JSON.stringify(reviewsFilm))
-                } else {
-                    let reviewsFilm = []
-                    reviewsFilm.push(x)
-                    localStorage.setItem(`reviewsFilm${idFilm}`, JSON.stringify(reviewsFilm))
-                }
-
-                localStorage.setItem('lengthRev', +x + 1)
-                arrayReviews.value.unshift(JSON.parse(localStorage.getItem(`Rev.${i}`)))
+            if (localStorage.getItem('logNAME')) {
+                userName = localStorage.getItem('logNAME')
+            } else {
+                userName = 'guest'
             }
+
+            let review = {
+                Name: userName,
+                ID: idFilm,
+                Description: descriptionRev
+            }
+
+            if (localStorage.getItem('lengthRev')) {
+                x = +localStorage.getItem('lengthRev')
+            } else {
+                localStorage.setItem('lengthRev', 0)
+
+            }
+            for (let i = 0; i < x + 1; ++i) {
+                if (!localStorage.getItem(`Rev.${i}`)) {
+
+                    localStorage.setItem(`Rev.${[i]}`, JSON.stringify(review))
+
+
+                    if (localStorage.getItem('logNAME')) {
+                        let reviewsUser = JSON.parse(localStorage.getItem(`reviews.${localStorage.getItem('logNAME')}`))
+                        reviewsUser.push(x)
+                        localStorage.setItem(`reviews.${localStorage.getItem('logNAME')}`, JSON.stringify(reviewsUser))
+                    }else{
+                        let reviewsUser = JSON.parse(localStorage.getItem(`reviews.${localStorage.getItem('guestID')}`))
+                        reviewsUser.push(x)
+                        localStorage.setItem(`reviews.${localStorage.getItem('guestID')}`, JSON.stringify(reviewsUser))
+                    }
+
+
+
+                    if (localStorage.getItem(`reviewsFilm${idFilm}`)) {
+                        let reviewsFilm = JSON.parse(localStorage.getItem(`reviewsFilm${idFilm}`))
+                        reviewsFilm.push(x)
+                        localStorage.setItem(`reviewsFilm${idFilm}`, JSON.stringify(reviewsFilm))
+                    } else {
+                        let reviewsFilm = []
+                        reviewsFilm.push(x)
+                        localStorage.setItem(`reviewsFilm${idFilm}`, JSON.stringify(reviewsFilm))
+                    }
+
+                    localStorage.setItem('lengthRev', +x + 1)
+                    arrayReviews.value.unshift(JSON.parse(localStorage.getItem(`Rev.${i}`)))
+                }
+            }
+
+            document.querySelector('.reviews__text_area').value = ''
+
         }
-
-        document.querySelector('.reviews__text_area').value = ''
-
     }
 }
